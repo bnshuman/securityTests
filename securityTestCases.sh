8cb2e72d115bf3ba3f28eb7540188fe823e407ba
Kelley Perry, Nina Bignalet, Rushita Patolia and Todd benson <pair+Kelley.Perry+Nina.Bignalet+Rushita.Patolia+Todd.Benson@csaa.com>
2016-11-10 11:36:50 -0700

    Changed the aguntlt image to reflect CSAA image

--- securityTestCases.sh
+++ securityTestCases.sh
@@ -1,8 +1,8 @@
-docker pull moomzni/gauntlt
-#docker run -e "HOSTNAME=e0.pncie.com" -v ~/securityTests/:/data moomzni/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=e1.pncie.com" -v ~/securityTests/:/data moomzni/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=e2.pncie.com" -v ~/securityTests/:/data moomzni/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=e3.pncie.com" -v ~/securityTests/:/data moomzni/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-#docker run -e "HOSTNAME=e4.pncie.com" -v ~/securityTests/:/data moomzni/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=csaa-insurance.aaa.com" -v ~/securityTests/:/data moomzni/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @prod"'
-docker run -e "HOSTNAME=quote.ds-csaa.io" -e "SQLMAP_PATH=/usr/local/bin/sqlmap" -v ~/securityTests/:/data moomzni/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @quote"'
+#docker pull csaa/gauntlt
+#docker run -e "HOSTNAME=e0.pncie.com" -v ~/securityTests/:/data csaa/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+docker run -e "HOSTNAME=e1.pncie.com" -v ~/securityTests/:/data csaa/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+docker run -e "HOSTNAME=e2.pncie.com" -v ~/securityTests/:/data csaa/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+docker run -e "HOSTNAME=e3.pncie.com" -v ~/securityTests/:/data csaa/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+#docker run -e "HOSTNAME=e4.pncie.com" -v ~/securityTests/:/data csaa/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+docker run -e "HOSTNAME=csaa-insurance.aaa.com" -v ~/securityTests/:/data csaa/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @prod"'
+docker run -e "HOSTNAME=quote.ds-csaa.io" -v ~/securityTests/:/data csaa/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @quote"'
