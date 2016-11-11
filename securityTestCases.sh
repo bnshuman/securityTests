9232ec89205743f719cc256ea63eb9457d7a235d
Todd Benson <todd.benson@csaa.com>
2016-11-11 15:49:56 -0700

    Update securityTestCases.sh

--- securityTestCases.sh
+++ securityTestCases.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-ls
 echo "start"
 #docker pull tgbenson/gauntlt
 #docker run -e "HOSTNAME=e0.pncie.com" -v $(pwd):/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
@@ -9,4 +8,4 @@ echo "start"
 #docker run -e "HOSTNAME=e4.pncie.com" -v $(pwd):/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
 #docker run -e "HOSTNAME=csaa-insurance.aaa.com" -v $(pwd):/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @prod"'
 #docker run -e "HOSTNAME=quote.ds-csaa.io" -v $(pwd):/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @quote"'
-#docker run -e "HOSTNAME=e1.pncie.com" /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+export HOSTNAME="e1.pncie.com" /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
