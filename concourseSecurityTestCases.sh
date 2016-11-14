9280d3a556c9f0704d1795a20c8536dd1f1e1cae
Todd Benson <todd.benson@csaa.com>
2016-11-14 13:48:30 -0700

    Update concourseSecurityTestCases.sh

--- concourseSecurityTestCases.sh
+++ concourseSecurityTestCases.sh
@@ -2,7 +2,7 @@
 #export HOSTNAME="e0.pncie.com" 
 #/bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
 export HOSTNAME="e1.pncie.com" 
-/bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+/bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf /opt/securityTests"'
 export HOSTNAME="e2.pncie.com"
 /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
 export HOSTNAME="e3.pncie.com"
