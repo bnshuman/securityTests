247ce1b6f69c3d9dc265f192ec3961978ff9cd7d
Kelley Perry, Nina Bignalet, Rushita Patolia and Todd benson <pair+Kelley.Perry+Nina.Bignalet+Rushita.Patolia+Todd.Benson@csaa.com>
2016-11-15 15:12:36 -0700

    Refactoring

--- concourseSecurityTestCases.sh
+++ concourseSecurityTestCases.sh
@@ -12,5 +12,5 @@ export HOSTNAME="e3.pncie.com"
 #/bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
 export HOSTNAME="csaa-insurance.aaa.com"
 /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @prod"'
-export HOSTNAME="quote.ds-csaa.io"
+# export HOSTNAME="quote.ds-csaa.io"
 /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @quote"'
