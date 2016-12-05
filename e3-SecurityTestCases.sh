09b471736815c26e976340592cbcb20d6a7edf7e
Todd Benson <todd.benson@csaa.com>
2016-12-05 14:43:50 -0700

    Update e3-SecurityTestCases.sh

--- e3-SecurityTestCases.sh
+++ e3-SecurityTestCases.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-cd /opt/securityTests
-export HOSTNAME="e3.pncie.com" 
-/bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
+/opt/gauntlt/bin/gauntlt -f progress -t @perf
