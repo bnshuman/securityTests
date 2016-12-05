c38b08a6fc53d901faa6a2620451e11564bb9499
Kelley Perry, Nina Bignalet, Rushita Patolia and Todd benson <pair+Kelley.Perry+Nina.Bignalet+Rushita.Patolia+Todd.Benson@csaa.com>
2016-12-05 09:27:33 -0700

    Changed quote URL

--- quote-SecurityTestCases.sh
+++ quote-SecurityTestCases.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 cd /opt/securityTests
-#export HOSTNAME="e0.pncie.com" 
-#/bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-export HOSTNAME="quote.ds-csaa.io"
+export HOSTNAME="quote.digital.pncie.com"
 /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @quote"'
