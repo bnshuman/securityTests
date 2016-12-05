65a2165f60661fd43ca0b35ad77d24fd68552f8b
Todd Benson <todd.benson@csaa.com>
2016-12-05 14:50:09 -0700

    Delete securityTestCases.sh

--- securityTestCases.sh
+++
@@ -1,8 +0,0 @@
-docker pull tgbenson/gauntlt
-#docker run -e "HOSTNAME=e0.pncie.com" -v ~/securityTests/:/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=e1.pncie.com" -v ~/securityTests/:/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=e2.pncie.com" -v ~/securityTests/:/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=e3.pncie.com" -v ~/securityTests/:/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-#docker run -e "HOSTNAME=e4.pncie.com" -v ~/securityTests/:/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @perf"'
-docker run -e "HOSTNAME=csaa-insurance.aaa.com" -v ~/securityTests/:/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @prod"'
-docker run -e "HOSTNAME=quote.ds-csaa.io" -v ~/securityTests/:/data tgbenson/gauntlt /bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @quote"'
