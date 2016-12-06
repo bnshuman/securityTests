0ff8dfaaa5cc86dc64c646f8b35e78920f53bb79
Todd Benson <todd.benson@csaa.com>
2016-12-06 12:15:16 -0700

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-cd /opt/securityTests
-export HOSTNAME="csaa-insurance.aaa.com"
-/bin/bash -c '/usr/bin/scl enable ruby193 python27 "gauntlt -f progress -t @prod"'
+export SSLYZE_PATH=/usr/bin/sslyze
+/opt/gauntlt/bin/gauntlt -f progress -t @prod
